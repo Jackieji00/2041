6d50d829f4589f9f80db308233a55db9a8791886
Peiqi Ji <842779063@qq.com>
2018-10-01 00:37:40 -0500

    a2 p4 finished

--- labs/lab03-code/colsum.ml
+++ labs/lab03-code/colsum.ml
@@ -3,15 +3,33 @@ let rec colsum_nt n =
   if n = 1 then
     1
   else
-    let next = 
-      if n mod 2 = 0 
+    let next =
+      if n mod 2 = 0
       then n/2
       else 3*n+1
     in
     let rest = colsum_nt next in
     n + rest
 ;;
-        
+
 let sum = colsum_nt 10 in
 Printf.printf "sum: %d\n" sum;
+;;
+
+let colsum_tr n =
+  let rec helper result input =
+    if input = 1 then
+      result+1
+    else
+      let input0 =
+        if input mod 2 = 0 then
+          input/2
+        else 3*input+1
+      in
+      let r = result+input in
+      helper r input0
+  in helper 0 n
+;;
+let sumtr = colsum_tr 10 in
+Printf.printf "sum: %d\n" sumtr;
 ;;
